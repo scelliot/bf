70ea81aa57445614ca28690881aa02c5f5edfcc8
Katharina <katharina@schallerl.com>
2018-06-24 23:05:46 +0200

    adds stopping countdown of current tour when no more seats are available

--- src/somecode.js
+++ src/somecode.js
@@ -107,6 +107,56 @@ function countDown(tour, group, gi){
     }
 }
 
+function timer(tour, group, gi){
+
+    // Set the date we're counting down to
+    var countDownDate = new Date(tour.endDate).getTime();
+
+    //check if date is valid. If not valid show an empty string
+    if (isNaN(countDownDate)) {
+        if (document.getElementById(tour.id) != null) {
+            document.getElementById(tour.id).innerHTML = " ";
+        }
+    } else {
+        // Update the count down every 1 second
+
+            // Get todays date and time
+            var now = new Date().getTime();
+
+            // Find the distance between now an the count down date
+            var distance = countDownDate - now;
+
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // Output the result in an element with id=tour.id
+            if (document.getElementById(tour.id) != null) {
+                document.getElementById(tour.id).innerHTML = days + "d " + hours + "h "
+                    + minutes + "m " + seconds + "s ";
+            }
+
+            // If the count down is over, do something
+            if (distance < 0) {
+                clearInterval(x);
+                if (document.getElementById(tour.id) != null) {
+                    document.getElementById(tour.id).innerHTML = "EXPIRED";
+                }
+                disableButtons(gi);
+                displayZeroSeatsLeft(gi);
+                if (document.getElementById("group" + gi) != null) {
+                    updateBackgroundColor(gi);
+                }
+            }
+    }
+}
+
+function stopCountdown(countdown) {
+    clearInterval(countdown);
+}
+
 const ul = document.getElementById('groups');
 const url = 'https://api.myjson.com/bins/1983ce';
 fetch(url)
@@ -164,8 +214,7 @@ fetch(url)
                 append(divTour, spanButton);
                 append(spanButton, bookNowButton);
 
-                countDown(tour, group, groupID);
-
+                let countdown = setInterval(function() {timer(tour, group, groupID);}, 1000);
 
                 function handleButtonClick() {
                     seats = seats - 1;
@@ -177,6 +226,7 @@ fetch(url)
                         disableButtons(groupID);
                         displayZeroSeatsLeft(groupID);
                         updateBackgroundColor(groupID);
+                        stopCountdown(countdown, groupID);
                     }
                 }
             });
